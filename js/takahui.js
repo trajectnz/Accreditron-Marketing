6ec99118e7c170cb3beb38934d35f89ee4bdb283
Ashlyn Baum <a@ashlynbaum.com>
2017-09-26 16:13:40 +1300

    refactor - upgrade to bootstrap 4

---
+++ js/takahui.js
@@ -0,0 +1,43 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Add smooth scrolling on all links inside the navbar
+    $("a.page-scroll").on('click', function(event) {
+
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+
+      } // End if
+
+    });
+
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: 100
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+
+})(jQuery); // End of use strict
